e844976e41c1a653432971bc59b78a42dcc1cc22
Юлия (Джулия) <sixquestions666@gmail.com>
2025-06-06 01:55:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ class Weapon(ABC):
         pass
 
 class Sword(Weapon):
+    name = "Меч"
     def attack(self):
-        print("Атака мечом")
+        print("Боец наносит удар мечом")
 
 class Bow(Weapon):
+    name = "Лук"
     def attack(self):
-        print("Атака луком")
+        print("Боец наносит удар луком")
 
 class Fighter:
     def __init__(self, name):
@@ -20,11 +22,11 @@ class Fighter:
 
     def change_weapon(self, weapon: Weapon):
         self.weapon = weapon
-        print(f"{self.name} changed {weapon.__class__.__name__}")
+        print(f"{self.name} выбирает {weapon.name}")
 
     def attack(self):
         if self.weapon:
-            print(self.weapon.attack())
+            (self.weapon.attack())
         else:
             print("Оружия нет")
 
@@ -37,4 +39,31 @@ class Monster:
         self.health = 0
         print(f"{self.name} побежден!")
 
-        
+def battle(fighter: Fighter, monster: Monster):
+        fighter.attack()
+        monster.take_damage()
+
+def weapon_name(weapon: Weapon):
+    names = {
+        'Sword': "меч",
+        'Bow': "лук"
+    }
+    return names.get(weapon.__class__.__name__, weapon.__class__.__name__)
+
+
+if __name__ == "__main__":
+    fighter = Fighter("Боец")
+    monster = Monster("Монстр")
+
+    sword = Sword()
+
+
+    fighter.change_weapon(sword)
+
+    battle(fighter, monster)
+    print()
+
+monster2 = Monster("Монстр 2")
+bow = Bow()
+fighter.change_weapon(bow)
+battle(fighter, monster2)
