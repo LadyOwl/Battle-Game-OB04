dd8209c0be3f2416a4da8c65ae5ec393c7c0be78
Юлия (Джулия) <sixquestions666@gmail.com>
2025-06-06 01:11:15 +0300

    add types of weapon and types of the fighters

--- main.py
+++ main.py
@@ -4,3 +4,37 @@ class Weapon(ABC):
     @abstractmethod
     def attack(self):
         pass
+
+class Sword(Weapon):
+    def attack(self):
+        print("Атака мечом")
+
+class Bow(Weapon):
+    def attack(self):
+        print("Атака луком")
+
+class Fighter:
+    def __init__(self, name):
+        self.name = "Боец"
+        self.weapon = None
+
+    def change_weapon(self, weapon: Weapon):
+        self.weapon = weapon
+        print(f"{self.name} changed {weapon.__class__.__name__}")
+
+    def attack(self):
+        if self.weapon:
+            print(self.weapon.attack())
+        else:
+            print("Оружия нет")
+
+class Monster:
+    def __init__(self, name):
+        self.name = "Монстр"
+        self.health = 100
+
+    def take_damage(self):
+        self.health = 0
+        print(f"{self.name} побежден!")
+
+        
